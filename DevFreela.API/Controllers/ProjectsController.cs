829fbf75e663d56a7053ef9c4ec3ca474c56347e
Hugo Victor <hugovic.it@gmail.com>
2024-01-08 10:44:14 -0300

    55. Controllers e Actions

---
+++ DevFreela.API/Controllers/ProjectsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevFreela.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProjectsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
