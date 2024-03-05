6b0d206d10fe4d28d8b970fb8a8cf3eb66240638
Hugo Victor <hugovic.it@gmail.com>
2024-03-04 21:11:17 -0300

    67. Camada Core - Parte 3

---
+++ DevFreela.Core/Entities/BaseEntities.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFreela.Core.Entities
+{
+    public abstract class BaseEntities
+    {
+        protected BaseEntities() { }
+        public int Id { get; private set; }
+
+    }
+}
